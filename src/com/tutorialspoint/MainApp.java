c840f43c9bfe5ba64c6071389a5ad3f7932f3487
ThangTran <thangminhtran2@gmail.com>
2018-11-06 10:39:01 +0900

    Initial project version

---
+++ src/com/tutorialspoint/MainApp.java
@@ -0,0 +1,53 @@
+package com.tutorialspoint;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		 ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
+//	      HelloWorld objA = (HelloWorld) context.getBean("helloWorld");
+//
+//	      objA.setMessage("I'm object A");
+//	      objA.getMessage();
+//
+//	      HelloWorld objB = (HelloWorld) context.getBean("helloWorld");
+//	      //objB.setMessage("I'm object B");
+//	      objB.getMessage();
+
+//		Spring - Bean Life Cycle
+//		AbstractApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
+//		HelloWorld obj = (HelloWorld) context.getBean("helloWorld");
+//		obj.getMessage();
+//		HelloWorld obj2 = (HelloWorld) context.getBean("tryTest");
+//		obj2.setMessage("I'm object 2");
+//		obj2.getMessage();
+//		context.registerShutdownHook();
+
+		// Spring - Bean Post Processors
+//		AbstractApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
+//
+//		HelloWorld obj = (HelloWorld) context.getBean("helloWorld");
+//		obj.getMessage();
+//		context.registerShutdownHook();
+
+		// Spring - Bean Definition Inheritance
+//		ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
+
+//		HelloWorld objA = (HelloWorld) context.getBean("helloWorld");
+//		objA.getMessage1();
+//		objA.getMessage2();
+
+//		HelloIndia objB = (HelloIndia) context.getBean("helloIndia");
+//		objB.getMessage1();
+//		objB.getMessage2();
+//		objB.getMessage3();
+		
+//		Spring - Dependency Injection là 1 phương pháp lập trình
+
+	}
+
+}
